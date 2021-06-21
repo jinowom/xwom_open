dd47150412a6b90fa90a26d61514227c8b9aae29
lzq <18418105@qq.com>
2021-06-21 08:43:28 +0800

    add  dashboard

--- environments/prod/backend/web/index.php
+++ environments/prod/backend/web/index.php
@@ -1,11 +1,14 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', false);
-defined('YII_ENV') or define('YII_ENV', 'prod');
+defined('YII_DEBUG') or define('YII_DEBUG', true);//开发时使用
+defined('YII_ENV') or define('YII_ENV', 'dev');//开发时使用
+//defined('YII_DEBUG') or define('YII_DEBUG', false);//生产交付时使用
+//defined('YII_ENV') or define('YII_ENV', 'prod');//生产交付时使用
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../../common/config/bootstrap.php';
 require __DIR__ . '/../config/bootstrap.php';
+require __DIR__ . '/../../common/helpers/function.php';
 
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../../common/config/main.php',
